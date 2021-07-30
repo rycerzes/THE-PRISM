176eecde97ddef97801923ee14f696aa5116b023
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-07-30 15:59:43 +0100

    Create avatar.ts

---
+++ src/commands/info/avatar.ts
@@ -0,0 +1,30 @@
+import { Command } from "#structures/Command";
+import type { Args, PieceContext } from "@sapphire/framework";
+import type { Message } from "discord.js";
+
+export default class extends Command {
+    constructor(context: PieceContext) {
+        super(context, {
+            name: 'avatar',
+            description: 'View a member\'s avatar',
+            usage: ['(member)'],
+            aliases: ['av'],
+            preconditions: ['guild']
+        });
+    };
+
+    public async run(message: Message, args: Args) {
+
+        const member = await args.pick('member').catch(() => message.member!) 
+
+        return message.reply({ allowedMentions: { repliedUser: false }, embeds: [
+            {
+                image: {
+                    url: member.user.displayAvatarURL({size: 1024})
+                },
+                color: await this.client.util.guildColor(message.guild!)
+            }
+        ]});
+
+    };
+};
