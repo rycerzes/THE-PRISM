8e7cb7076c1222a9f600d372dd6fe4064777e4e1
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-08-06 19:29:24 +0100

    Create moderator.ts

---
+++ src/preconditions/moderator.ts
@@ -0,0 +1,23 @@
+import { Precondition } from '#structures/Precondition';
+import type { PieceContext, PreconditionOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+export default class extends Precondition {
+
+    constructor(context: PieceContext, { ...options }: PreconditionOptions) {
+        super(context, {
+            name: 'moderator',
+            ...options
+        });
+    };
+
+    public async run(message: Message) {
+
+        if (!message.guild) return this.ok();
+
+        const { mod_role_id } = await this.db.fetchGuild(message.guild);
+        
+        return mod_role_id ? message.member!.roles.cache.has(mod_role_id) ? this.ok() : this.error({ message: 'This command is Moderator only.' }) : this.error({ message: 'This command is Moderator only.' });
+        
+    };
+};
