40881003f9e3528a8b2e5a8538a8334f28f2f924
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-08-06 19:29:29 +0100

    Update Listener.ts

--- src/lib/structures/Listener.ts
+++ src/lib/structures/Listener.ts
@@ -1,6 +1,14 @@
 import Sapphire from '@sapphire/framework';
+import type { Guild } from 'discord.js';
+import type { BaseModule } from './BaseModule';
 
 export abstract class Listener extends Sapphire.Listener {
+
+    public async checkModule(guild: Guild, moduleResolveable: string | number | BaseModule): Promise<boolean> {
+
+        return (await this.client.getModuleManager(guild)).isEnabled(moduleResolveable)
+
+    };
 
     get client() {
         return this.container.client;
