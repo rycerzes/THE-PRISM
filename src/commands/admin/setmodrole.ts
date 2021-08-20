6e6c54408be88b944ab5685ba614517ad44ef6ed
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-08-20 16:22:36 +0100

    Added modrole

---
+++ src/commands/admin/setmodrole.ts
@@ -0,0 +1,56 @@
+import { Command } from "#structures/Command";
+import { colors } from "#util/config";
+import type { Args, PieceContext } from "@sapphire/framework";
+import type { Message } from "discord.js";
+
+export default class extends Command {
+    constructor(context: PieceContext) {
+        super(context, {
+            name: 'setmodrole',
+            description: 'Set moderator role',
+            usage: ['[role]', 'remove'],
+            aliases: ['modrole'],
+            preconditions: ['admin', 'guild']
+        });
+    };
+
+    public async run(message: Message, args: Args) {
+        
+        const peeked = await args.peekResult('string');
+
+        if (['remove', 'rm', 'rem'].includes(peeked.value?.toLowerCase() || '')) {
+            
+            await this.db.updateGuild(message.guild!, `SET mod_role_id = null`);
+
+            return message.reply({ allowedMentions: { repliedUser: false }, embeds: [
+                {
+                    title: 'MOD ROLE REMOVED',
+                    color: colors.red,
+                    timestamp: Date.now()
+                }
+            ]});
+
+        } else {
+
+            const role = (await args.pickResult('role')).value;
+
+            if (role) {
+
+                await this.db.updateGuild(message.guild!, `SET mod_role_id = ${role.id}`);
+
+                return message.reply({ allowedMentions: { repliedUser: false }, embeds: [
+                    {
+                        title: 'MOD ROLE UPDATED',
+                        description: `${role}`,
+                        color: await this.client.util.guildColor(message.guild!),
+                        timestamp: Date.now()
+                    }
+                ]});
+
+
+            } else return message.reply({ allowedMentions: { repliedUser: false }, content: 'Unable to resolve role' })
+
+        };
+
+    };
+};
