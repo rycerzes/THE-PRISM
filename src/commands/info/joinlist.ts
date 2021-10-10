1e74176d8a56f7f63c4f21f17e8d24c10f37000c
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-10-10 23:31:49 +0100

    Update joinlist.ts

--- src/commands/info/joinlist.ts
+++ src/commands/info/joinlist.ts
@@ -25,7 +25,7 @@ export default class extends Command {
             channel: message.channel as GuildTextBasedChannelTypes,
             author: message.author,
             array: [...(await message.guild!.members.fetch()).values()],
-            perPage: 15,
+            perPage: 10,
             buttons: true,
 
             title: `${message.guild!.name.toUpperCase()} JOIN RANKINGS`,
