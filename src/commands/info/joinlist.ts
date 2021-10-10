3a3c4041cb187dbaf7dc26543927dbb06df0caf3
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-10-10 23:29:01 +0100

    Update joinlist.ts

--- src/commands/info/joinlist.ts
+++ src/commands/info/joinlist.ts
@@ -31,7 +31,7 @@ export default class extends Command {
             title: `${message.guild!.name.toUpperCase()} JOIN RANKINGS`,
 
             display: (member: GuildMember) => {
-                return `${member} • <t:${Math.floor(member.joinedTimestamp!/1000)}>`
+                return `<t:${Math.floor(member.joinedTimestamp!/1000)}:D> • ${member} • <t:${Math.floor(member.joinedTimestamp!/1000)}:t>`
             },
             sort: (a: GuildMember, b: GuildMember) => {
                 return a.joinedTimestamp! - b.joinedTimestamp!
